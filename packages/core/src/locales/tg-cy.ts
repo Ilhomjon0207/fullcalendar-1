232d8d8f15a12016992d4b6d0480200e87d7ab95
Ilhomjon Qodorov <111631902+ilhomjon-dev@users.noreply.github.com>
2026-05-01 16:17:49 +0500

    feat: add Tajik locale

---
+++ packages/core/src/locales/tg-cy.ts
@@ -0,0 +1,22 @@
+import { LocaleInput } from '../index.js'
+
+const tg: LocaleInput = {
+  code: 'tg',
+
+  buttonText: {
+    prev: 'Қаблӣ',
+    next: 'Баъдӣ',
+    today: 'Имрӯз',
+    month: 'Моҳ',
+    week: 'Ҳафта',
+    day: 'Рӯз',
+    list: 'Рӯйхат'
+  },
+
+  weekText: 'Ҳафта',
+  allDayText: 'Тамоми рӯз',
+  moreLinkText: (n) => `+${n} боз`,
+  noEventsText: 'Ягон рӯйдод нест'
+}
+
+export default tg
